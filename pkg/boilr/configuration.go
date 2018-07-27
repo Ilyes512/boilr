dda7d487f09a89684fd3fbfb8e52bc3aa7b767ec
Ilyes512 <ilyes.ahidar@gmail.com>
2018-07-28 00:19:11 +0200

    Release: v0.4.3

--- pkg/boilr/configuration.go
+++ pkg/boilr/configuration.go
@@ -16,7 +16,7 @@ const (
 	AppName = "boilr"
 
 	// Version of the application
-	Version = "0.4.2"
+	Version = "0.4.3"
 
 	// ConfigDirPath is the configuration directory of the application
 	ConfigDirPath = ".config/boilr"
